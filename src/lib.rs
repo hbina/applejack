25813a52eca7152a5d744dc08e0e36558d939033
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2021-01-19 21:19:07 +0800

    Cleaned up
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
+#[allow(unused_macros)]
+// The debug version
+#[cfg(debug_assertions)]
+macro_rules! log {
+    ($( $args:expr ),*) => { println!( $( $args ),* ); }
+}
+
+// Non-debug version
+
+#[cfg(not(debug_assertions))]
+macro_rules! log {
+    ($( $args:expr ),*) => {
+        ()
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use crate::TrieNode;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn tryyy() {
+    fn general_tests() {
         let mut trie = TrieNode::new();
         trie.insert(&[0, 1, 2]);
         println!("1. trie:{:#?}", trie);
@@ -20,10 +31,80 @@ mod tests {
         println!("4. trie:{:#?}", trie);
         trie.insert(&[]);
         println!("5. trie:{:#?}", trie);
+        assert_eq!(
+            trie,
+            TrieNode {
+                prefix: vec![],
+                branches: vec![TrieNode {
+                    prefix: vec![0, 1],
+                    branches: vec![
+                        TrieNode {
+                            prefix: vec![2],
+                            branches: vec![TrieNode {
+                                prefix: vec![3, 4],
+                                branches: vec![]
+                            },]
+                        },
+                        TrieNode {
+                            prefix: vec![3],
+                            branches: vec![]
+                        }
+                    ]
+                },]
+            }
+        )
+    }
+
+    #[test]
+    fn insert_empty() {
+        let mut trie = TrieNode::new();
+        trie.insert(&[0, 1, 2]);
+        trie.insert(&[]);
+        trie.insert(&[0, 1, 2, 3, 4]);
+        trie.insert(&[0, 1, 2, 3, 4, 5, 6]);
+        assert_eq!(
+            trie,
+            TrieNode {
+                prefix: vec![],
+                branches: vec![TrieNode {
+                    prefix: vec![0, 1, 2],
+                    branches: vec![TrieNode {
+                        prefix: vec![3, 4],
+                        branches: vec![TrieNode {
+                            prefix: vec![5, 6],
+                            branches: vec![]
+                        }]
+                    }]
+                },]
+            }
+        )
+    }
+
+    #[test]
+    fn insert_very_different_strings() {
+        let mut trie = TrieNode::new();
+        trie.insert(&[0, 1, 2, 3]);
+        trie.insert(&[4, 5, 6, 7]);
+        assert_eq!(
+            trie,
+            TrieNode {
+                prefix: vec![],
+                branches: vec![
+                    TrieNode {
+                        prefix: vec![0, 1, 2, 3],
+                        branches: vec![]
+                    },
+                    TrieNode {
+                        prefix: vec![4, 5, 6, 7],
+                        branches: vec![]
+                    }
+                ]
+            }
+        )
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Cut {
     Parent(usize),
     Child(usize),
@@ -32,97 +113,84 @@ enum Cut {
     BothEnd,
 }
 
-#[derive(Debug)]
-pub struct TrieBranch {
-    prefix: Vec<u8>,
-    nodes: Vec<TrieNode>,
-}
-
-impl TrieBranch {
-    pub fn new(key: &[u8]) -> TrieBranch {
-        TrieBranch {
-            prefix: Vec::from(key),
-            nodes: vec![],
-        }
-    }
-
-    pub fn with_nodes(prefix: Vec<u8>, nodes: Vec<TrieNode>) -> TrieBranch {
-        TrieBranch { prefix, nodes }
-    }
-}
-
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct TrieNode {
-    branches: Vec<TrieBranch>,
+    prefix: Vec<u8>,
+    branches: Vec<TrieNode>,
 }
 
 impl TrieNode {
     pub fn new() -> TrieNode {
-        TrieNode { branches: vec![] }
-    }
-
-    pub fn from_branches(branches: Vec<TrieBranch>) -> TrieNode {
-        TrieNode { branches }
-    }
-
-    pub fn insert(&mut self, new_key: &[u8]) {
-        // Use some other enum to make this unrepresentable
-        let cut = self.branches.iter().enumerate().find_map(|(i, t)| {
-            if let (Some(l), Some(r)) = (t.prefix.iter().next(), new_key.iter().next()) {
-                if l == r {
-                    Some(i)
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        });
-        if let Some(i) = cut {
-            let TrieBranch { prefix, nodes } = &mut self.branches[i];
-            match cut_key(prefix, new_key) {
-                Cut::Parent(p) => {
-                    let drained_key = prefix.drain(p..).collect();
-                    let drained_branch = nodes.drain(..).collect();
-                    nodes.push(TrieNode::from_branches(vec![TrieBranch::with_nodes(
-                        drained_key,
-                        drained_branch,
-                    )]));
-                }
-                Cut::Child(c) => nodes.push(TrieNode::from_branches(vec![TrieBranch::with_nodes(
-                    Vec::from(&new_key[c + 1..]),
-                    vec![],
-                )])),
-                Cut::BothBegin => {
-                    unreachable!()
-                }
-                Cut::BothMiddle(p) => {
-                    let drained_key = prefix.drain(p..).collect();
-                    let drained_children = nodes.drain(..).collect();
-                    nodes.push(TrieNode::from_branches(vec![TrieBranch::with_nodes(
-                        drained_key,
-                        drained_children,
-                    )]));
-                    nodes.push(TrieNode::from_branches(vec![TrieBranch::with_nodes(
-                        Vec::from(&new_key[p..]),
-                        vec![],
-                    )]));
-                }
-                Cut::BothEnd => {
-                    // Just write in-place
-                }
-            }
-        } else {
-            self.branches
-                .push(TrieBranch::with_nodes(Vec::from(new_key), vec![]));
+        TrieNode {
+            prefix: vec![],
+            branches: vec![],
         }
+    }
+
+    pub fn with_key(key: &[u8]) -> TrieNode {
+        TrieNode {
+            prefix: Vec::from(key),
+            branches: vec![],
+        }
+    }
+
+    pub fn with_branches(branches: Vec<TrieNode>) -> TrieNode {
+        TrieNode {
+            prefix: vec![],
+            branches,
+        }
+    }
+
+    pub fn with_key_and_branches(key: &[u8], branches: Vec<TrieNode>) -> TrieNode {
+        TrieNode {
+            prefix: Vec::from(key),
+            branches,
+        }
+    }
+
+    pub fn insert(&mut self, new_key: &[u8]) -> bool {
+        let cut = cut_key(&self.prefix, new_key);
+        log!(
+            "inserting\nkey:{:?}\nto self:{:#?}\ncut:{:?}",
+            new_key,
+            self,
+            cut
+        );
+        match cut {
+            Cut::Parent(p) => {
+                let drained_key = self.prefix.drain(p..).collect();
+                let drained_branch = self.branches.drain(..).collect();
+                self.branches.push(TrieNode::with_branches(vec![TrieNode {
+                    prefix: drained_key,
+                    branches: drained_branch,
+                }]));
+            }
+            Cut::Child(c) => {
+                let cut_child = &new_key[c..];
+                if !self.branches.iter_mut().any(|x| x.insert(cut_child)) {
+                    self.branches.push(TrieNode::with_key(cut_child));
+                }
+            }
+            Cut::BothBegin => return false,
+            Cut::BothMiddle(p) => {
+                let drained_key = self.prefix.drain(p..).collect::<Vec<_>>();
+                let drained_children = self.branches.drain(..).collect();
+                self.branches.push(TrieNode::with_key_and_branches(
+                    &drained_key,
+                    drained_children,
+                ));
+                self.branches
+                    .push(TrieNode::with_key_and_branches(&new_key[p..], vec![]));
+            }
+            Cut::BothEnd => {}
+        }
+        true
     }
 }
 
 fn cut_key<'a, 'b>(prefix_key: &'a [u8], child_key: &'b [u8]) -> Cut {
     let idx = prefix_key.iter().zip(child_key).position(|(l, r)| l != r);
     if let Some(idx) = idx {
-        // SAFETY: The substraction below is safe because idx is guaranteed to be smaller than both lengths
         if idx == 0 {
             Cut::BothBegin
         } else {
@@ -131,9 +199,9 @@ fn cut_key<'a, 'b>(prefix_key: &'a [u8], child_key: &'b [u8]) -> Cut {
     } else {
         let (llen, clen) = (prefix_key.len(), child_key.len());
         match llen.cmp(&clen) {
-            std::cmp::Ordering::Less => Cut::Child(clen - llen),
+            std::cmp::Ordering::Less => Cut::Child(llen),
             std::cmp::Ordering::Equal => Cut::BothEnd,
-            std::cmp::Ordering::Greater => Cut::Parent(llen - clen),
+            std::cmp::Ordering::Greater => Cut::Parent(clen),
         }
     }
 }
