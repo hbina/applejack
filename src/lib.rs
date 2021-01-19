511c018446a356d597ed0f014fb99ba8f1f8ec66
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2021-01-19 22:14:23 +0800

    Added tests and simplified code even more
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
-#[allow(unused_macros)]
-// The debug version
-#[cfg(debug_assertions)]
-macro_rules! log {
-    ($( $args:expr ),*) => { println!( $( $args ),* ); }
-}
-
-// Non-debug version
-
-#[cfg(not(debug_assertions))]
-macro_rules! log {
-    ($( $args:expr ),*) => {
-        ()
-    };
-}
-
 #[cfg(test)]
 mod tests {
     use crate::TrieNode;
@@ -22,15 +6,10 @@ mod tests {
     fn general_tests() {
         let mut trie = TrieNode::new();
         trie.insert(&[0, 1, 2]);
-        println!("1. trie:{:#?}", trie);
         trie.insert(&[0, 1, 2, 3, 4]);
-        println!("2. trie:{:#?}", trie);
         trie.insert(&[0, 1, 3]);
-        println!("3. trie:{:#?}", trie);
         trie.insert(&[]);
-        println!("4. trie:{:#?}", trie);
         trie.insert(&[]);
-        println!("5. trie:{:#?}", trie);
         assert_eq!(
             trie,
             TrieNode {
@@ -102,6 +81,65 @@ mod tests {
             }
         )
     }
+
+    #[test]
+    fn get_something_that_exist() {
+        let mut trie = TrieNode::new();
+        trie.insert(&[0, 1, 2, 3]);
+        trie.insert(&[4, 5, 6, 7]);
+        assert_eq!(
+            trie,
+            TrieNode {
+                prefix: vec![],
+                branches: vec![
+                    TrieNode {
+                        prefix: vec![0, 1, 2, 3],
+                        branches: vec![]
+                    },
+                    TrieNode {
+                        prefix: vec![4, 5, 6, 7],
+                        branches: vec![]
+                    }
+                ]
+            }
+        );
+        assert!(trie.get(&[0, 1, 2, 3]));
+    }
+
+    #[test]
+    fn get_empty_exists() {
+        let trie = TrieNode::new();
+        assert!(trie.get(&[]));
+    }
+
+    #[test]
+    fn get_nested_exists() {
+        let mut trie = TrieNode::new();
+        trie.insert(&[0, 1, 2]);
+        trie.insert(&[]);
+        trie.insert(&[0, 1, 2, 3, 4]);
+        trie.insert(&[0, 1, 2, 3, 4, 5, 6]);
+        assert_eq!(
+            trie,
+            TrieNode {
+                prefix: vec![],
+                branches: vec![TrieNode {
+                    prefix: vec![0, 1, 2],
+                    branches: vec![TrieNode {
+                        prefix: vec![3, 4],
+                        branches: vec![TrieNode {
+                            prefix: vec![5, 6],
+                            branches: vec![]
+                        }]
+                    }]
+                },]
+            }
+        );
+        assert!(!trie.get(&[0, 1, 2, 3]));
+        assert!(trie.get(&[0, 1, 2, 3, 4]));
+        assert!(!trie.get(&[0, 1, 2, 3, 4, 5]));
+        assert!(trie.get(&[0, 1, 2, 3, 4, 5, 6]));
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -149,13 +187,7 @@ impl TrieNode {
     }
 
     pub fn insert(&mut self, new_key: &[u8]) -> bool {
-        let cut = cut_key(&self.prefix, new_key);
-        log!(
-            "inserting\nkey:{:?}\nto self:{:#?}\ncut:{:?}",
-            new_key,
-            self,
-            cut
-        );
+        let cut = self.cut_key(new_key);
         match cut {
             Cut::Parent(p) => {
                 let drained_key = self.prefix.drain(p..).collect();
@@ -186,22 +218,32 @@ impl TrieNode {
         }
         true
     }
-}
 
-fn cut_key<'a, 'b>(prefix_key: &'a [u8], child_key: &'b [u8]) -> Cut {
-    let idx = prefix_key.iter().zip(child_key).position(|(l, r)| l != r);
-    if let Some(idx) = idx {
-        if idx == 0 {
-            Cut::BothBegin
-        } else {
-            Cut::BothMiddle(idx)
+    pub fn get(&self, key: &[u8]) -> bool {
+        match self.cut_key(key) {
+            Cut::Parent(_) => false,
+            Cut::Child(idx) => self.branches.iter().any(|x| x.get(&key[idx..])),
+            Cut::BothBegin => false,
+            Cut::BothMiddle(_) => false,
+            Cut::BothEnd => true,
         }
-    } else {
-        let (llen, clen) = (prefix_key.len(), child_key.len());
-        match llen.cmp(&clen) {
-            std::cmp::Ordering::Less => Cut::Child(llen),
-            std::cmp::Ordering::Equal => Cut::BothEnd,
-            std::cmp::Ordering::Greater => Cut::Parent(clen),
+    }
+
+    fn cut_key<'b>(&self, child_key: &'b [u8]) -> Cut {
+        let idx = self.prefix.iter().zip(child_key).position(|(l, r)| l != r);
+        if let Some(idx) = idx {
+            if idx == 0 {
+                Cut::BothBegin
+            } else {
+                Cut::BothMiddle(idx)
+            }
+        } else {
+            let (llen, clen) = (self.prefix.len(), child_key.len());
+            match llen.cmp(&clen) {
+                std::cmp::Ordering::Less => Cut::Child(llen),
+                std::cmp::Ordering::Equal => Cut::BothEnd,
+                std::cmp::Ordering::Greater => Cut::Parent(clen),
+            }
         }
     }
 }
