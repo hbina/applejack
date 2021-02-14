e1a766e9a1c51f8c1d6014fe0175c0153eecd2aa
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2021-02-14 13:25:21 +0800

    Attempting to split. Its kinda tough...
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use minivec::{mini_vec, MiniVec};
 use smallvec::{smallvec, SmallVec};
 
 type Key = SmallVec<[u8; 8]>;
@@ -13,18 +12,46 @@ enum Cut {
 }
 
 #[derive(PartialEq, Debug)]
+enum InsertType<'a, T> {
+    Parent(&'a mut TrieNode<T>, usize, usize),
+    Middle(&'a mut TrieNode<T>, usize, usize),
+    Inplace(&'a mut TrieNode<T>, usize),
+    Append(&'a mut TrieNode<T>, usize),
+}
+
+impl Cut {
+    pub fn cut(parent_key: &[u8], child_key: &[u8]) -> Cut {
+        let idx = parent_key.iter().zip(child_key).position(|(l, r)| l != r);
+        if let Some(idx) = idx {
+            if idx == 0 {
+                Cut::BothBegin
+            } else {
+                Cut::BothMiddle(idx)
+            }
+        } else {
+            let (plen, clen) = (parent_key.len(), child_key.len());
+            match plen.cmp(&clen) {
+                std::cmp::Ordering::Less => Cut::Child(plen),
+                std::cmp::Ordering::Equal => Cut::BothEnd,
+                std::cmp::Ordering::Greater => Cut::Parent(clen),
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 pub struct TrieNode<T> {
-    pub(crate) value: Option<T>,
-    pub(crate) prefix: Key,
-    pub(crate) branches: MiniVec<TrieNode<T>>,
+    pub keys: Vec<Key>,
+    pub nodes: Vec<TrieNode<T>>,
+    pub values: Vec<Option<T>>,
 }
 
 impl<T> Default for TrieNode<T> {
     fn default() -> Self {
         TrieNode {
-            value: None,
-            prefix: smallvec![],
-            branches: mini_vec![],
+            keys: vec![],
+            nodes: vec![],
+            values: vec![],
         }
     }
 }
@@ -33,390 +60,123 @@ impl<T> TrieNode<T>
 where
     T: std::fmt::Debug,
 {
+    fn find_mut(&mut self, new_key: &[u8], key_idx: usize) -> InsertType<T> {
+        println!("inserting> new_key:{:?} key_idx:{}", new_key, key_idx);
+        for idx in 0..self.keys.len() {
+            let key = &self.keys[idx];
+            let child_key = &new_key[key_idx..];
+            println!("child_key:{:?}", child_key);
+            match Cut::cut(key, child_key) {
+                Cut::Parent(p) => return InsertType::Parent(self, idx, p),
+                Cut::Child(c) => return self.nodes[idx].find_mut(new_key, key_idx + c),
+                Cut::BothBegin => continue,
+                Cut::BothMiddle(m) => return InsertType::Middle(self, idx, m),
+                Cut::BothEnd => return InsertType::Inplace(self, idx),
+            }
+        }
+        return InsertType::Append(self, key_idx);
+    }
+
     pub fn insert(&mut self, new_key: &[u8], value: T) {
-        self.insert_impl(new_key, &mut Some(value));
+        self.insert_impl(new_key, Some(value))
     }
 
-    fn insert_impl(&mut self, new_key: &[u8], value: &mut Option<T>) -> bool {
-        let cut = self.cut_key(new_key);
-        match cut {
-            Cut::Parent(p) => {
-                let drained_value = self.value.take();
-                self.value = value.take();
-                let drained_key = self.prefix.drain(p..).collect::<Key>();
-                let drained_branch = self.branches.drain(..).collect();
-                self.branches.push(TrieNode {
-                    value: drained_value,
-                    prefix: drained_key,
-                    branches: drained_branch,
+    pub fn insert_impl(&mut self, new_key: &[u8], value: Option<T>) {
+        let node = self.find_mut(new_key, 0);
+        println!("found node> node:{:#?}", node);
+        match node {
+            InsertType::Parent(parent_node, idx, p) => {
+                let drained_key = parent_node.keys[idx].drain(p..).collect::<Key>();
+                let drained_nodes = parent_node.nodes[idx].nodes.drain(..).collect::<Vec<_>>();
+                let drained_value = parent_node.values[idx].take();
+                parent_node.nodes[idx].nodes.push(TrieNode {
+                    keys: vec![drained_key],
+                    nodes: drained_nodes,
+                    values: vec![drained_value],
                 });
-                true
+                parent_node.values[idx] = value;
             }
-            Cut::Child(c) => {
-                let cut_child = &new_key[c..];
-                let found = self
-                    .branches
-                    .iter_mut()
-                    .any(|x| x.insert_impl(cut_child, value));
-                if !found {
-                    self.branches.push(TrieNode {
-                        value: value.take(),
-                        prefix: Key::from(cut_child),
-                        branches: mini_vec![],
-                    });
-                }
-                true
+            InsertType::Middle(parent_node, idx, m) => {
+                let drained_key = parent_node.keys[idx].drain(m..).collect::<Key>();
+                let drained_value = parent_node.values[idx].take();
+                parent_node.nodes[idx].insert_impl(&drained_key, drained_value);
+                parent_node.nodes[idx].insert_impl(&new_key[m..], value);
             }
-            Cut::BothBegin => false,
-            Cut::BothMiddle(p) => {
-                let drained_value = dbg!(self.value.take());
-                let drained_key = dbg!(self.prefix.drain(p..).collect::<Key>());
-                let drained_children = dbg!(self.branches.drain(..).collect());
-                self.branches.push(TrieNode {
-                    value: drained_value,
-                    prefix: drained_key,
-                    branches: drained_children,
-                });
-                self.branches.push(TrieNode {
-                    value: value.take(),
-                    prefix: Key::from(&new_key[p..]),
-                    branches: mini_vec![],
-                });
-                true
+            InsertType::Inplace(parent_node, idx) => {
+                parent_node.values[idx] = value;
             }
-            Cut::BothEnd => {
-                self.value = value.take();
-                true
+            InsertType::Append(parent_node, k) => {
+                parent_node.keys.push(Key::from_slice(&new_key[k..]));
+                parent_node.nodes.push(TrieNode::default());
+                parent_node.values.push(value);
             }
         }
     }
 
-    pub fn exists(&self, key: &[u8]) -> bool {
-        match self.cut_key(key) {
-            Cut::Parent(_) => false,
-            Cut::Child(idx) => self.branches.iter().any(|x| x.exists(&key[idx..])),
-            Cut::BothBegin => false,
-            Cut::BothMiddle(_) => false,
-            Cut::BothEnd => true,
-        }
+    pub fn exists(&self, child_key: &[u8]) -> bool {
+        self.keys
+            .iter()
+            .any(|parent_key| match Cut::cut(parent_key, child_key) {
+                Cut::Parent(_) => false,
+                Cut::Child(idx) => self.nodes.iter().any(|x| x.exists(&parent_key[idx..])),
+                Cut::BothBegin => false,
+                Cut::BothMiddle(_) => false,
+                Cut::BothEnd => true,
+            })
     }
+}
 
-    fn cut_key<'b>(&self, child_key: &'b [u8]) -> Cut {
-        let idx = self.prefix.iter().zip(child_key).position(|(l, r)| l != r);
-        if let Some(idx) = idx {
-            if idx == 0 {
-                Cut::BothBegin
-            } else {
-                Cut::BothMiddle(idx)
-            }
-        } else {
-            let (llen, clen) = (self.prefix.len(), child_key.len());
-            match llen.cmp(&clen) {
-                std::cmp::Ordering::Less => Cut::Child(llen),
-                std::cmp::Ordering::Equal => Cut::BothEnd,
-                std::cmp::Ordering::Greater => Cut::Parent(clen),
-            }
-        }
-    }
+#[test]
+fn small_tests() {
+    let mut trie = TrieNode::default();
+    println!("trie1:{:#?}", trie);
+    trie.insert(&[0, 1, 2], ());
+    println!("trie2:{:#?}", trie);
+    trie.insert(&[0, 1, 2, 3, 4], ());
+    println!("trie3:{:#?}", trie);
 }
 
 #[test]
 fn general_tests() {
     let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2], ());
-    trie.insert(&[0, 1, 2, 3, 4], ());
-    trie.insert(&[0, 1, 3], ());
-    trie.insert(&[], ());
-    trie.insert(&[], ());
+    println!("trie1:{:#?}", trie);
+    trie.insert(&[0, 1, 2], 1);
+    println!("trie2:{:#?}", trie);
+    trie.insert(&[0, 1, 2, 3, 4], 2);
+    println!("trie3:{:#?}", trie);
+    trie.insert(&[0, 1, 3], 3);
+    println!("trie4:{:#?}", trie);
+    trie.insert(&[], 4);
+    println!("trie5:{:#?}", trie);
+    trie.insert(&[], 5);
+    println!("trie6:{:#?}", trie);
+    assert_eq!(
+        trie,
+        TrieNode {
+            keys: vec![smallvec![]],
+            nodes: vec![TrieNode {
+                keys: vec![smallvec![0, 1]],
+                nodes: vec![
+                    TrieNode {
+                        keys: vec![smallvec![2], smallvec![3]],
+                        nodes: vec![TrieNode {
+                            keys: vec![smallvec![3, 4]],
+                            nodes: vec![TrieNode::default()],
+                            values: vec![Some(2)],
+                        }],
+                        values: vec![Some(1), Some(3)]
+                    },
+                    TrieNode::default()
+                ],
+                values: vec![None]
+            }],
+            values: vec![Some(5)]
+        }
+    );
+
     assert!(trie.exists(&[0, 1, 2]));
     assert!(trie.exists(&[0, 1, 2, 3, 4]));
     assert!(trie.exists(&[0, 1, 3]));
     assert!(trie.exists(&[]));
     assert!(!trie.exists(&[1, 2, 3]));
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: Some(()),
-            prefix: smallvec![],
-            branches: mini_vec![TrieNode {
-                value: None,
-                prefix: smallvec![0, 1],
-                branches: mini_vec![
-                    TrieNode {
-                        value: Some(()),
-                        prefix: smallvec![2],
-                        branches: mini_vec![TrieNode {
-                            value: Some(()),
-                            prefix: smallvec![3, 4],
-                            branches: mini_vec![]
-                        },]
-                    },
-                    TrieNode {
-                        value: Some(()),
-                        prefix: smallvec![3],
-                        branches: mini_vec![]
-                    }
-                ]
-            },]
-        }
-    )
-}
-
-#[test]
-fn insert_empty() {
-    let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2], ());
-    trie.insert(&[], ());
-    trie.insert(&[0, 1, 2, 3, 4], ());
-    trie.insert(&[0, 1, 2, 3, 4, 5, 6], ());
-    assert!(trie.exists(&[0, 1, 2]));
-    assert!(trie.exists(&[]));
-    assert!(!trie.exists(&[0, 1, 2, 3]));
-    assert!(!trie.exists(&[0, 1]));
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: Some(()),
-            prefix: smallvec![],
-            branches: mini_vec![TrieNode {
-                value: Some(()),
-                prefix: smallvec![0, 1, 2],
-                branches: mini_vec![TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![3, 4],
-                    branches: mini_vec![TrieNode {
-                        value: Some(()),
-                        prefix: smallvec![5, 6],
-                        branches: mini_vec![]
-                    }]
-                }]
-            },]
-        }
-    )
-}
-
-#[test]
-fn insert_very_different_strings() {
-    let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2, 3], ());
-    trie.insert(&[4, 5, 6, 7], ());
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: None,
-            prefix: smallvec![],
-            branches: mini_vec![
-                TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![0, 1, 2, 3],
-                    branches: mini_vec![]
-                },
-                TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![4, 5, 6, 7],
-                    branches: mini_vec![]
-                }
-            ]
-        }
-    )
-}
-
-#[test]
-fn get_something_that_exist() {
-    let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2, 3], ());
-    trie.insert(&[4, 5, 6, 7], ());
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: None,
-            prefix: smallvec![],
-            branches: mini_vec![
-                TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![0, 1, 2, 3],
-                    branches: mini_vec![]
-                },
-                TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![4, 5, 6, 7],
-                    branches: mini_vec![]
-                }
-            ]
-        }
-    );
-    assert!(trie.exists(&[0, 1, 2, 3]));
-}
-
-#[test]
-fn initialize_with_something_big() {
-    let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2, 3], ());
-    trie.insert(&[0, 1, 2, 3, 4], ());
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: None,
-            prefix: smallvec![],
-            branches: mini_vec![TrieNode {
-                value: Some(()),
-                prefix: smallvec![0, 1, 2, 3],
-                branches: mini_vec![TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![4],
-                    branches: mini_vec![]
-                },]
-            }]
-        }
-    );
-    assert!(trie.exists(&[0, 1, 2, 3]));
-}
-
-#[test]
-fn get_empty_exists() {
-    let trie = TrieNode::<()>::default();
-    assert!(trie.exists(&[]));
-}
-
-#[test]
-fn get_nested_exists() {
-    let mut trie = TrieNode::default();
-    trie.insert(&[0, 1, 2], ());
-    trie.insert(&[], ());
-    trie.insert(&[0, 1, 2, 3, 4], ());
-    trie.insert(&[0, 1, 2, 3, 4, 5, 6], ());
-    assert_eq!(
-        trie,
-        TrieNode {
-            value: Some(()),
-            prefix: smallvec![],
-            branches: mini_vec![TrieNode {
-                value: Some(()),
-                prefix: smallvec![0, 1, 2],
-                branches: mini_vec![TrieNode {
-                    value: Some(()),
-                    prefix: smallvec![3, 4],
-                    branches: mini_vec![TrieNode {
-                        value: Some(()),
-                        prefix: smallvec![5, 6],
-                        branches: mini_vec![]
-                    }]
-                }]
-            },]
-        }
-    );
-    assert!(!trie.exists(&[0, 1, 2, 3]));
-    assert!(trie.exists(&[0, 1, 2, 3, 4]));
-    assert!(!trie.exists(&[0, 1, 2, 3, 4, 5]));
-    assert!(trie.exists(&[0, 1, 2, 3, 4, 5, 6]));
-}
-
-#[test]
-fn assert_size_of_node() {
-    assert_eq!(48, std::mem::size_of::<TrieNode<()>>());
-}
-
-#[test]
-fn test_fuzzy_input_1() {
-    let input = [
-        vec![114],
-        vec![114],
-        vec![109],
-        vec![244],
-        vec![40],
-        vec![66],
-        vec![2],
-        vec![0],
-        vec![38],
-        vec![137],
-        vec![3],
-        vec![31],
-        vec![222],
-        vec![64],
-        vec![61],
-        vec![46],
-        vec![33],
-        vec![245],
-        vec![128],
-        vec![42],
-        vec![243],
-        vec![188],
-        vec![165],
-        vec![224],
-        vec![82],
-        vec![37],
-        vec![232],
-        vec![73],
-        vec![196],
-        vec![240],
-        vec![168],
-        vec![131],
-        vec![36],
-        vec![59],
-        vec![25],
-        vec![129],
-        vec![17],
-        vec![1],
-        vec![239],
-        vec![105],
-        vec![221],
-        vec![39],
-        vec![47],
-        vec![44],
-        vec![152],
-        vec![250],
-        vec![149],
-        vec![14],
-        vec![205],
-        vec![223],
-        vec![255],
-        vec![72],
-        vec![93, 254],
-        vec![31, 25],
-        vec![31, 25],
-        vec![6, 0],
-        vec![143, 222],
-        vec![49, 140],
-        vec![0, 1],
-        vec![0, 1],
-        vec![2, 27],
-        vec![2, 27],
-        vec![9, 25],
-        vec![255, 223],
-        vec![255, 223],
-        vec![37, 25],
-        vec![37, 25],
-        vec![230, 45],
-        vec![0, 25],
-        vec![42, 96],
-        vec![42, 96],
-        vec![1, 0],
-        vec![1, 0],
-        vec![0, 20],
-        vec![2, 45],
-        vec![255, 255],
-        vec![244, 244],
-        vec![244, 244],
-        vec![45, 36],
-        vec![135, 25],
-        vec![28, 0],
-        // The next line will crash
-        vec![230, 85],
-    ];
-    let mut trie = TrieNode::default();
-    for x in input.iter() {
-        trie.insert(&x, ());
-    }
-    println!("trie:\n{:#?}", trie);
-    assert!(input.iter().all(|x| { trie.exists(x) }));
-}
-
-#[test]
-fn test_fuzzy_input_1_minimized() {
-    let input = [vec![230, 45], vec![230, 85]];
-    let mut trie = TrieNode::default();
-    for x in input.iter() {
-        trie.insert(&x, ());
-    }
-    assert!(input.iter().all(|x| { trie.exists(x) }));
 }
